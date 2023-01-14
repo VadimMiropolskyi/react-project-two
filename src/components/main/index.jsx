afa90158bd247b7d0cf8bf92bec7e86febc1c070
wadim.mir0602 <wadim.mir0602@gmail.com>
2023-01-14 10:43:40 +0100

    add 3 Input's

---
+++ src/components/main/index.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './main.css';
+
+class Main extends Component {
+render() {
+    return (
+        <>
+        <header>
+            <div><input type ='button' value ='1'/></div>
+            <div><input type ='button' value ='2'/></div>
+            <div><input type ='button' value ='3'/></div>
+        </header>
+        
+        <section>
+        
+
+        </section>
+        </>
+    )
+}
+
+}
+
+/*https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json*/
+
+export default Main;
